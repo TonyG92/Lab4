26df805c5b760389a16b94cdb51b7ec09c41a204
Antonio Angotti <Angotti9@gmail.com>
2019-04-25 16:39:42 -0400

    This is Lab 4

---
+++ Lab4/Methods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Methods
+    {
+        public static int GetInt(string message)
+        {
+            Console.Write(message);
+            int number = int.Parse(Console.ReadLine());
+            return number;
+        }
+    }
+}
---
+++ Lab4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Learn your squares and cubes!: ");
+            
+            string response = "y";
+            
+            while(response == "y")
+            {
+                int num1 = Methods.GetInt("\nEnter an integer: ");
+                Console.WriteLine("Number\tSquared\tCubed");
+                Console.WriteLine("======\t======\t======");
+
+                for (int i = 1; i <= num1; i++)
+                {
+                    Console.WriteLine($"{i} \t {i * i} \t {i * i * i}");
+                }
+
+                Console.WriteLine("\nContinue? (y/n): ");
+                response = Console.ReadLine();
+            }
+           
+
+
+
+        }
+    }
+}
